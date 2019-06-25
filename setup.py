10b27197db40bf671ee82500a4b817ac33e35f10
Shirui <2361394+Shirui816@users.noreply.github.com>
2019-06-25 10:37:38 +0800

    first commit

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 ext_modules = [
-    Extension("func",
-              ["func.pyx"],
+    Extension("u",
+              ["u.pyx"],
               libraries=["m"],
               extra_compile_args=["-O3", "-ffast-math", "-march=native", "-fopenmp"],
               extra_link_args=['-fopenmp']
@@ -13,7 +13,7 @@ ext_modules = [
 ]
 
 setup(
-    name="func",
+    name="u",
     cmdclass={"build_ext": build_ext},
     ext_modules=ext_modules
 )
