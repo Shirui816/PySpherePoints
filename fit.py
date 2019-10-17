ce7a32ec48a4f8958d881cdd7acef213a8c85cca
施呵呵 <2361394+Shirui816@users.noreply.github.com>
2019-10-18 04:50:50 +0800

    Update fit.py
    Adding Ellipsoid support

--- fit.py
+++ fit.py
@@ -42,6 +42,26 @@ def n3(x):  # fastest, if N is small, i.e. N << 1000 particles
     x = x / np.linalg.norm(x, axis=1)[:, None]
     return np.sum(1 / distance.pdist(x))
 
+# x, y, z -> x/a, y/b, z/c
+# let x' = (x/a) / sqrt((x/a)^2+(y/b)^2+(z/c)^2) then
+# a x' = x / r with r = sqrt((x/a)^2+(y/b)^2+(z/c)^2)
+# The function first map x -> x / ratio then
+# normalize the (x/a, y/b, z/c) vector -> x', y', z'; afterwards
+# energy is calculated as u((x', y',z') \times ratio).
+# Therefore the derivative is calculated as
+# D[1/sqrt((u-a x')^2+(v-b y')^2+(w-c z')^2), x]
+# let f = 1/sqrt((u-a x')^2+(v-b y')^2+(w-c z')^2) then
+# df/dx = df/dx' dx'/dx + df/dy' dy'/dx + df/dz' dz'/dx
+
+
+def n4(x, ratio):  # Ellipsoid support
+    x = x.reshape(-1, 3)
+    norm = np.linalg.norm(x/ratio, axis=1)
+    #x = x / np.linalg.norm(x, axis=1)[:, None]
+    #x = x * ratio
+    return uf(x, norm, ratio)
+
+
 
 # x0 = (N\times 3,) points, for scipy.optimize.minimize takes 1-D array as input.
 res = minimize(n3, x0=np.random.random((300,)))
